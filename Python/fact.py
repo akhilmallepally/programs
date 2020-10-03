3f026e1197e18bd47cb86489b125a57505f10eca
Akhil <akhil@swecha.net>
2020-10-03 19:27:18 +0530

    Upload New File

---
+++ Python/fact.py
@@ -0,0 +1,11 @@
+num = int(input("enter number : "))
+fact = 1
+for i in range(1, num+1):
+    if (num == 0):
+        print(fact)
+    else:
+        fact = fact*i
+print(fact)
+
+
+#try writing in while loop
