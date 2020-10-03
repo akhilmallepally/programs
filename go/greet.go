b06cb2de3ce61d423c1f20597262192f78641f02
Akhil <akhil@swecha.net>
2020-10-03 16:56:33 +0530

    Update greet.go

--- go/greet.go
+++ go/greet.go
@@ -1,13 +1,18 @@
+
+/*In this program we will know how to use assign a constant and a variable. Variable can be assigned in different ways. We will explore the other way in next examples.
+
+And don't worry about other syntax. Just ignore as of now.
+*/
+
 package main
 
 import "fmt"
 
 func main(){
-    const greet string
-    greet = "Hi, How are you doing"
+    const greet = "Hi, How are you doing"
     fmt.Println(greet)
 
     var good string
     fmt.Scan(&good)
-    fmt.Printf("I am %v ", good)
+    fmt.Printf("I am doing %v ", good)
 }
