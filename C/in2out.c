7c3cb65a6cda74469d34f6657d171b1f8db03448
Akhil <akhil@swecha.net>
2021-01-19 06:33:05 +0530

    Update in2out.c

--- C/in2out.c
+++ C/in2out.c
@@ -1,1 +1,32 @@
+/*getchar() : is a library function that reads the next input character and returns that as its value.
 
+c = getchar();  The variable c contains the next character of the input
+
+
+putchar() : is a library function that prints a character each time that is called. */
+
+#include <stdio.h>
+
+void main(){
+    int c;
+    c = getchar();     
+    while(c!=EOF){
+        putchar(c);
+        c = getchar();
+    }
+}
+
+
+//The below is another method where we give the character input directly at the while loop
+
+/*
+#include <stdio.h>
+
+void main(void){
+    int c;
+    while((c = getchar()!=EOF){
+        putchar(c);
+    }
+}
+
+/*
