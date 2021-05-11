406fd9c582aee6786e94f48c275ec9a7820c8ce7
akhilmallepally <akhilnani831@gmail.com>
2021-05-11 00:43:39 -0500

    new program

---
+++ Python/arrays/sum.py
@@ -0,0 +1,12 @@
+
+def sum(a):
+    """Sum of elements present in an array"""
+    sum = 0
+    for i in a:
+        sum = sum+i
+    print(sum)
+a = [1,2,3,4,5]
+sum(a)
+
+#or use a sum function
+#    print(sum(a))
