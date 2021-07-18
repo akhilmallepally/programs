236c1bbe523721c8a717ad29703bc308e9ad5f2d
akhilmallepally <akhilnani831@gmail.com>
2021-07-17 22:01:42 -0500

    added loops programs

---
+++ Python/beginner-programs/loops/while/first_n_numbers.py
@@ -0,0 +1,5 @@
+n = int(input("enter the number : "))
+i =0
+while(i<=n):
+    print(i)
+    i+=1
---
+++ Python/beginner-programs/loops/while/sum.py
@@ -0,0 +1,7 @@
+num = int(input("enter the number : "))
+i = 0
+sum = 0
+while(i<=num):
+    sum = sum + i
+    i = i+1
+print(sum)
