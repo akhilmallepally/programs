93d82a870477ef87bbd9655d075c5535e39f28e3
Akhil Mallepally <akhilnani831@gmail.com>
2021-10-03 18:52:36 -0500

    Create singlylinkedlist.py
    insertion at beginning, end, search, delete operations

---
+++ Python/Data Structures/singlylinkedlist.py
@@ -0,0 +1,71 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        
+    def display(self):
+        if self.head == None:
+            print("List Empty")
+            return
+        
+        current = self.head
+        while current:
+            print(current.data)
+            current = current.next
+    
+    def insert_at_end(self, data):
+        new_node = Node(data)
+        if self.head != None:
+            current = self.head
+            while current.next != None:
+                current = current.next
+            current.next = new_node
+        else:
+            self.head = new_node
+    def insert_at_begin(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+    
+    def delete_node(self, ele):
+        if self.head == None:
+            print("List empty")
+            return
+        if self.head.data  == ele:
+            temp = self.head
+            self.head = temp.next
+            temp = None
+            return
+        current = self.head
+        while current.next != None:
+            if current.next.data == ele:
+                temp = current.next
+                current.next = temp.next
+                temp = None
+                return
+            current= current.next
+        print("Element is not found in our list")
+        
+    def search(self, ele):
+        if self.head == None:
+            print("List is empty")
+            return
+        current = self.head
+        while current != None:
+            if current.data == ele:
+                print("Element found")
+                return
+            current = current.next
+            
+        
+llist = LinkedList()
+llist.insert_at_end(40)
+llist.insert_at_begin(10)
+llist.insert_at_begin(5)
+llist.display()
+llist.delete_node(40)
+llist.display()
